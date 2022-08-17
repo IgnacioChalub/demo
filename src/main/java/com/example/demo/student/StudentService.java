fe70493d81981fdf1bb7dd8e81b01e26e9f0064b
IgnacioChalub <ignaciochalub@gmail.com>
2022-08-17 00:27:50 -0300

    get students from db

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,24 +1,22 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Ignacio",
-                        "Ignacio@mail.com",
-                        LocalDate.of(2000, Month.JULY, 5),
-                        20
-                )
-        );
+        return studentRepository.findAll();
     }
 
 }
